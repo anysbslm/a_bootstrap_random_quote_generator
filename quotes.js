e18f9a85ae2f2ec838f44622c57a6a66fb62d9e9
Mohamed Anis Bousselmi <anys.bslm@gmail.com>
2022-12-22 15:00:09 +0100

    add random quote generator

---
+++ quotes.js
@@ -0,0 +1,64 @@
+    const QUOTES = [
+        {
+            quote:"Don't use your brain to play it, let your feelings guide your fingers.",
+            author:"Jimi Hendrix"
+        },
+        {quote:"It’s not the years in your life that count. It’s the life in your years.",
+        author:"Abraham Lincoln"},
+{
+       quote:"Change your thoughts and you change your world.",
+       author:"Norman Vincent Peale"},
+{
+       quote:"Either write something worth reading or do something worth writing.",
+       author:"Benjamin Franklin"},
+{
+       quote:"Nothing is impossible, the word itself says, “I’m possible!”",
+       author:"–Audrey Hepburn"},
+{
+       quote:"The only way to do great work is to love what you do.",
+       author:"Steve Jobs"},
+{
+       quote:"If you can dream it, you can achieve it.",
+       author:"Zig Ziglar"},
+
+        {
+            quote:"Tell the children the truth.",
+            author:"Bob Marley"
+        },
+        {
+            quote:"I am proud of where I come from and never forget the people I grew up with.",
+            author:"Zinedine Zidane"
+        },
+        {
+            quote:"Education is the most powerful weapon which you can use to change the world.",
+            author:"Nelson Mandela"
+        },
+        {
+            quote:"When we lose our principles, we invite chaos.",
+            author:"Mr.Robot"
+        },
+        {
+            quote:"I have no sense of humor about losing",
+            author:"Rafael Nadal"
+        },
+        {
+            quote:"It's never too late to be what you might've been.",
+            author:"George Eliot"
+        },
+        {
+            quote:"If you can dream it, you can do it.",
+            author:"Walt Disney",
+        },
+        {
+            quote:"Trust yourself that you can do it and get it.",
+            author:"Baz Luhrmann"
+        },
+        {
+            quote:"If people are doubting how far you can go, go so far that you can’t hear them anymore.",
+            author:"Michele Ruiz"
+        },
+        {
+            quote:"Only the paranoid survive.",
+            author:"Andy Grove"
+        },
+    ]
